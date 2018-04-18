6a0564c1b09357e6b4a12c864c55103fe1ac5f43
dexter <chetanmadan24@gmail.com>
2018-04-18 11:26:23 +0530

    Added four words to crossword. converted textview array to matrix

--- app/src/main/java/com/pcr/dexter/crossword/MainActivity.java
+++ app/src/main/java/com/pcr/dexter/crossword/MainActivity.java
@@ -22,50 +22,53 @@ public class MainActivity extends AppCompatActivity {
     public ArrayList<String> words = new ArrayList<>();
 
 
-    private TextView[] textviews = new TextView[37];
+    private TextView[][] textviews = new TextView[6][6];
     Random rand = new Random();
+
+    public MainActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textviews[0] = (TextView) findViewById(R.id.textView1);
-        textviews[1] = (TextView) findViewById(R.id.textView2);
-        textviews[2] = (TextView) findViewById(R.id.textView3);
-        textviews[3] = (TextView) findViewById(R.id.textView4);
-        textviews[4] = (TextView) findViewById(R.id.textView5);
-        textviews[5] = (TextView) findViewById(R.id.textView6);
-        textviews[6] = (TextView) findViewById(R.id.textView7);
-        textviews[7] = (TextView) findViewById(R.id.textView8);
-        textviews[8] = (TextView) findViewById(R.id.textView9);
-        textviews[9] = (TextView) findViewById(R.id.textView10);
-        textviews[10] = (TextView) findViewById(R.id.textView11);
-        textviews[11] = (TextView) findViewById(R.id.textView12);
-        textviews[12] = (TextView) findViewById(R.id.textView13);
-        textviews[13] = (TextView) findViewById(R.id.textView14);
-        textviews[14] = (TextView) findViewById(R.id.textView15);
-        textviews[15] = (TextView) findViewById(R.id.textView16);
-        textviews[16] = (TextView) findViewById(R.id.textView17);
-        textviews[17] = (TextView) findViewById(R.id.textView18);
-        textviews[18] = (TextView) findViewById(R.id.textView19);
-        textviews[19] = (TextView) findViewById(R.id.textView20);
-        textviews[20] = (TextView) findViewById(R.id.textView21);
-        textviews[21] = (TextView) findViewById(R.id.textView22);
-        textviews[22] = (TextView) findViewById(R.id.textView23);
-        textviews[23] = (TextView) findViewById(R.id.textView24);
-        textviews[24] = (TextView) findViewById(R.id.textView25);
-        textviews[25] = (TextView) findViewById(R.id.textView26);
-        textviews[26] = (TextView) findViewById(R.id.textView27);
-        textviews[27] = (TextView) findViewById(R.id.textView28);
-        textviews[28] = (TextView) findViewById(R.id.textView29);
-        textviews[29] = (TextView) findViewById(R.id.textView30);
-        textviews[30] = (TextView) findViewById(R.id.textView31);
-        textviews[31] = (TextView) findViewById(R.id.textView32);
-        textviews[32] = (TextView) findViewById(R.id.textView33);
-        textviews[33] = (TextView) findViewById(R.id.textView34);
-        textviews[34] = (TextView) findViewById(R.id.textView35);
-        textviews[35] = (TextView) findViewById(R.id.textView36);
+        textviews[0][0] = (TextView) findViewById(R.id.textView1);
+        textviews[1][0] = (TextView) findViewById(R.id.textView2);
+        textviews[2][0] = (TextView) findViewById(R.id.textView3);
+        textviews[3][0] = (TextView) findViewById(R.id.textView4);
+        textviews[4][0] = (TextView) findViewById(R.id.textView5);
+        textviews[5][0] = (TextView) findViewById(R.id.textView6);
+        textviews[0][1] = (TextView) findViewById(R.id.textView7);
+        textviews[1][1] = (TextView) findViewById(R.id.textView8);
+        textviews[2][1] = (TextView) findViewById(R.id.textView9);
+        textviews[3][1] = (TextView) findViewById(R.id.textView10);
+        textviews[4][1] = (TextView) findViewById(R.id.textView11);
+        textviews[5][1] = (TextView) findViewById(R.id.textView12);
+        textviews[0][2] = (TextView) findViewById(R.id.textView13);
+        textviews[1][2] = (TextView) findViewById(R.id.textView14);
+        textviews[2][2] = (TextView) findViewById(R.id.textView15);
+        textviews[3][2] = (TextView) findViewById(R.id.textView16);
+        textviews[4][2] = (TextView) findViewById(R.id.textView17);
+        textviews[5][2] = (TextView) findViewById(R.id.textView18);
+        textviews[0][3] = (TextView) findViewById(R.id.textView19);
+        textviews[1][3] = (TextView) findViewById(R.id.textView20);
+        textviews[2][3] = (TextView) findViewById(R.id.textView21);
+        textviews[3][3] = (TextView) findViewById(R.id.textView22);
+        textviews[4][3] = (TextView) findViewById(R.id.textView23);
+        textviews[5][3] = (TextView) findViewById(R.id.textView24);
+        textviews[0][4] = (TextView) findViewById(R.id.textView25);
+        textviews[1][4] = (TextView) findViewById(R.id.textView26);
+        textviews[2][4] = (TextView) findViewById(R.id.textView27);
+        textviews[3][4] = (TextView) findViewById(R.id.textView28);
+        textviews[4][4] = (TextView) findViewById(R.id.textView29);
+        textviews[5][4] = (TextView) findViewById(R.id.textView30);
+        textviews[0][5] = (TextView) findViewById(R.id.textView31);
+        textviews[1][5] = (TextView) findViewById(R.id.textView32);
+        textviews[2][5] = (TextView) findViewById(R.id.textView33);
+        textviews[3][5] = (TextView) findViewById(R.id.textView34);
+        textviews[4][5] = (TextView) findViewById(R.id.textView35);
+        textviews[5][5] = (TextView) findViewById(R.id.textView36);
 
         AssetManager assetManager = getAssets();
         try {
@@ -84,82 +87,194 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-        String some = "";
-        
-        while (some.length() != 6) {
-            some = words.get(rand.nextInt(words.size()));
+        String startWord = "";
+
+        while (startWord.length() != 6) {
+            startWord = words.get(rand.nextInt(words.size()));
 
         }
-        char[] chsome = some.toCharArray();
-        char c = some.charAt(3);
+        char[] chStartWord = startWord.toCharArray();
         int i;
-        int r = rand.nextInt(6);
-        int row = 0;
-        switch (r) {
-            case 1:
-                row = 0;
+        int row = rand.nextInt(5);
+        int column = rand.nextInt(5);
+
+        setwordVert(0, 0, startWord);
+
+
+        String c2 = "" + textviews[0][2].getText();
+        String w2 = "";
+
+        for (i = 0; i < words.size(); i++) {
+            w2 = words.get(i);
+            String cht = "" + w2.charAt(0);
+            if (Character.toString(w2.charAt(0)).equalsIgnoreCase(c2) && w2.length() == 6) {
                 break;
-            case 2:
-                row = 6;
+            } else if (Character.toString(w2.charAt(0)).equalsIgnoreCase(c2) && w2.length() == 5) {
                 break;
-            case 3:
-                row = 12;
-                break;
-            case 4:
-                row = 18;
-                break;
-            case 5:
-                row = 24;
-                break;
-            case 6:
-                row = 30;
-                break;
+            } else {
+
+            }
         }
 
-        for (i = 0; i < 6; i++, row++) {
-            textviews[row].setText(Character.toString(chsome[i]).toUpperCase());
+
+        String c5 = "" + textviews[0][5].getText();
+        String w5 = "";
+        for (i = 0; i < words.size(); i++) {
+            w5 = words.get(i);
+            if (Character.toString(w5.charAt(0)).equalsIgnoreCase(c5) && w5.length() == 6 && w5 != w2) {
+                break;
+            } else if (Character.toString(w5.charAt(0)).equalsIgnoreCase(c5) && w5.length() == 5 && w5 != w2) {
+                break;
+            }
+
+
+        }
+        setwordHoriz(0, 2, w2);
+        setwordHoriz(0, 5, w5);
+
+
+
+
+
+
+        String r5 = "" + textviews[5][5].getText();
+        String wr5 = "";
+        String temp = ""+ textviews[5][2].getText();
+        if(textviews[5][3].getText()==null) {
+            for (i = 0; i < words.size(); i++) {
+                wr5 = words.get(i);
+                if (Character.toString(w5.charAt(w5.length() - 1)).equalsIgnoreCase(r5) && wr5.length() == 6) {
+                    break;
+                } else if (Character.toString(w5.charAt(0)).equalsIgnoreCase(r5) && wr5.length() == 5) {
+                    break;
+                }
+                else if (Character.toString(w5.charAt(wr5.length() - 1)).equalsIgnoreCase(r5) && Character.toString(wr5.charAt(2)).equalsIgnoreCase(temp) && wr5.length() == 4) {
+                    break;
+                }
+            }
+        }
+        else{
+            for (i = 0; i < words.size(); i++) {
+                wr5 = words.get(i);
+                if (Character.toString(w5.charAt(w5.length() - 1)).equalsIgnoreCase(r5) && Character.toString(wr5.charAt(2)).equalsIgnoreCase(String.valueOf(textviews[5][2].getText())) && wr5.length() == 6) {
+                    break;
+                } else if (Character.toString(w5.charAt(w5.length() - 1)).equalsIgnoreCase(r5) && Character.toString(wr5.charAt(2)).equalsIgnoreCase(String.valueOf(textviews[5][2].getText())) && wr5.length() == 5) {
+                    break;
+                }
+                else if (Character.toString(w5.charAt(w5.length() - 1)).equalsIgnoreCase(r5) && Character.toString(wr5.charAt(2)).equalsIgnoreCase(temp) && wr5.length() == 4) {
+                    break;
+                }
+            }
+            }
+        setwordVert(5, 0, wr5);
+
+
+
+
+
+
+
+
+
+
+/*
+
+        String c11 = "" + textviews[0][5].getText();
+        String c12 = "" + textviews[0][0].getText();
+        String wr1 = "";
+        for (i = 0; i < words.size(); i++) {
+            wr1 = words.get(i);
+            if (Character.toString(w5.charAt(0)).equalsIgnoreCase(c5) && w5.length() == 6 && w5 != w2) {
+                break;
+            } else if (Character.toString(w5.charAt(0)).equalsIgnoreCase(c2) && w5.length() == 5 && w5 != w2) {
+                break;
+            }
+
+
+        }
+  */  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void setwordHoriz(int rowStart, int columnStart, String wordToPrint) {
+            int i = 0;
+
+            char[] chStartWord = wordToPrint.toCharArray();
+
+            for (i = rowStart; i < wordToPrint.length(); i++) {
+                textviews[i][columnStart].setText(Character.toString(chStartWord[i]));
+            }
         }
 
+
+
+    private void setwordVert(int rowStart, int columnStart, String wordToPrint) {
+        int i = 0;
+
+        char[] chStartWord = wordToPrint.toCharArray();
+
+        for (i = columnStart; i < wordToPrint.length(); i++) {
+            textviews[rowStart][i].setText(Character.toString(chStartWord[i]));
+        }
     }
 
-    private int check() {
-        int i=0;
-        int j=0;
-        while (i++ % 6 != 0) {
-            if (textviews[i] == null) {
-                i++;
-            } else {
-                j= i;
-            }
 
-            //return (j);
+
+    private int checkRow(int i,int j) {
+        int k=i;
+        while (k<5) {
+            if (textviews[k+1][j].getText() == null) {
+                k++;
+            } else if(textviews[k+1][j].getText() != null){
+                return k;
+            }
         }
-        while (i-- % 6 != 0) {
-            if (textviews[i] == null) {
+
+        while (i>0) {
+            if (textviews[i-1][j].getText() == null) {
                 i--;
-            } else {
-                j= i;
+            }
+            else if(textviews[i-1][j].getText() != null){
+                return j;
+            }
+        }
+        return 0;
+    }
+
+
+    private int checkColumn(int i,int j){
+        int k = j;
+        while (k >0) {
+            if (textviews[i][k-1].getText() == null) {
+                k = k - 1;
+            }
+            else if(textviews[i][k].getText() != null){
+                k= i;
+                return k;
             }
         }
 
-
-
-        while (i > 6) {
-            if (textviews[i] == null) {
-                i = i - 6;
-            } else {
-                j= i;
+        while (j<5) {
+            if (textviews[i][j+1].getText() == null) {
+                j = j + 1;
+            } else if(textviews[i][j].getText() != null){
+                return j;
             }
         }
-
-        while (i<=36) {
-            if (textviews[i] == null) {
-                i = i + 6;
-            } else {
-                j= i;
-            }
-        }
-        return j;
+        return 0;
 
     }
 
